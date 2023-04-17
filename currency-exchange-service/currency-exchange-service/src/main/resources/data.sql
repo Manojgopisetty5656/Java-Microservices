550339ea1ed7bc8a3a7705f58a22f1cfa5367d91
Manoj Gopisetty <85223503+Manojgopisetty5656@users.noreply.github.com>
2023-04-17 11:57:17 +0530

    Delete currency-exchange-service/currency-exchange-service directory

--- currency-exchange-service/currency-exchange-service/src/main/resources/data.sql
+++
@@ -1,8 +0,0 @@
-insert into Currency_Exchange(id,currency_from,currency_to,conversion_multiple,environment)
-values(10001,'USD','INR',65.2,'');
-
-insert into Currency_Exchange(id,currency_from,currency_to,conversion_multiple,environment)
-values(10002,'EUR','INR',66.2,'');
-
-insert into Currency_Exchange(id,currency_from,currency_to,conversion_multiple,environment)
-values(10003,'AUD','INR',67.2,'');
